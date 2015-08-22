bfcb8caefbcfa18b35bff3cb6fe2267c6a96ae80
Megan Searles <megan.d.searles@gmail.com>
2015-08-21 21:27:23 -0600

    PROGRAM IS WORKING GREAT YAY

--- everything.py
+++ everything.py
@@ -16,11 +16,12 @@ api = tweepy.API(auth)
 #		Make a list of the user ids of those the replies are to
 #		Remove those who are not mutuals
 #		See if they also have replies to the main user
-#		(Write a function to do these things, so you can reuse it)
+#		  (Write a function to do these things, so you can reuse it)
 #		Set a minimum for something to gauge interaction level
+#		  Or: set up a ranking system
 #		Maybe look at faves and RTs?
 
-init_user = api.get_user('christieclimb') # Get main user's information. Could probably be made to prompt for a user and then run program
+init_user = api.get_user('ashlaraee002') # Get main user's information. Could probably be made to prompt for a user and then run program
 init_id = init_user.id
 
 init_follower_count = init_user.followers_count
@@ -47,7 +48,8 @@ def pullReplies(user_in, list_in):
 		max = list_in[-1].id - 1
 		time.sleep(5)
 	for tweet in list_in:
-		if tweet.in_reply_to_user_id is None: # Note: Doesn't remove all Nones, but can be filtered out later by other means
+		# Note: Doesn't remove all Nones for some reason, but can be filtered out later by other means
+		if tweet.in_reply_to_user_id is None: 
 			list_in.remove(tweet)
 	
 init_list = []
@@ -70,13 +72,42 @@ for secondary in init_list:
 init_tweets = []
 pullReplies(init_id, init_tweets)
 
-mutual_replies = []
+init_replies = []
 for tweet in init_tweets:
 	# If the reply is to a mutual and is not already in the list, then it is added to the list of mutuals the main user replies to
-	# Note: Change screen_name to user_id later - this was just to test
-	if tweet.in_reply_to_user_id in mutual_list and tweet.in_reply_to_screen_name not in mutual_replies:
-		mutual_replies.append(tweet.in_reply_to_screen_name)
+	if tweet.in_reply_to_user_id in mutual_list and tweet.in_reply_to_user_id not in init_replies:
+		init_replies.append(tweet.in_reply_to_user_id)
+	
+# Checks if secondary user also replies to main user - Should there be something more?
+# Didn't work with commented out section, so I fixed it up in a more roundabout way
+mutual_replies = []
+placeholder = [init_id]
+for secondary in init_replies:
+	sec_tweets = []
+	pullReplies(secondary, sec_tweets)
+	for tweet in sec_tweets:
+		if tweet.in_reply_to_user_id in placeholder and secondary not in mutual_replies:
+			mutual_replies.append(secondary)
+	#answer = any(tweet.in_reply_to_user_id is init_id for tweet in sec_tweets)
+	#if answer:
+	#	mutual_replies.append(secondary)
+	
+init_faves = api.favorites(user_id=init_id,count=200)
+time.sleep(60)
+init_fave_ids = []	
+for fave in init_faves:
+	if fave.user.id in mutual_list and fave.user.id not in init_fave_ids:
+		init_fave_ids.append(fave.user.id)
+		
+print init_fave_ids
 
-# Gives feedback as to whether the program worked
-for item in mutual_replies:
-	print str(item)
+mutual_fave_ids = []
+for secondary in init_fave_ids:
+	sec_faves = api.favorites(user_id=secondary,count=200)
+	for fave in sec_faves:
+		if fave.user.id in placeholder and secondary not in mutual_fave_ids:
+			mutual_fave_ids.append(secondary)
+	time.sleep(60)
+	
+print mutual_fave_ids
+
