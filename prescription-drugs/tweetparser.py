1bad36a6d14325126023052707323a42d46fb50e
megansearles <megan.d.searles@gmail.com>
2016-12-08 20:02:11 -0700

    Making sure everything's up-to-date

--- prescription-drugs/tweetparser.py
+++ prescription-drugs/tweetparser.py
@@ -1,5 +1,7 @@
 import json
 
+unique_users = []
+
 with open('12May2016.json') as data_file:
     for line in data_file:    
         data = json.loads(line)
@@ -22,7 +24,27 @@ with open('12May2016.json') as data_file:
             in_reply_to_status_id = 0
             in_reply_to_user_id_str = "Null"
             in_reply_to_user_id = 0
-        coordinates = data["coordinates"]["coordinates"]
-#TypeError: 'NoneType' object has no attribute '__getitem__'
-        print coordinates
-#        place_id = ["place"]["id"]
+        try:
+            coordinates = data["coordinates"]["coordinates"]
+        except TypeError:
+            coordinates = "Null"
+        try:
+            place_id = data["place"]["id"]
+        except TypeError:
+            place_id = "Null"
+        
+        if user_id not in unique_users:
+            user_id_str = data["user"]["id_str"]
+            protected = data["user"]["protected"]
+            screen_name = data["user"]["screen_name"]
+            verified = data["user"]["verified"]
+            statuses_count = data["user"]["statuses_count"]
+            try:
+                description = data["description"]
+            except KeyError:
+                description = "Null"
+            try:
+                location = data["user"]["location"]
+            except KeyError:
+                location = "Null"
+            unique_users.append(user_id)
